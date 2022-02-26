cff65944d75c14362e205d2318d20a729b7e3112
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2022-02-27 02:11:40 +0600

    delete button add

--- js/apps.js
+++ js/apps.js
@@ -1,5 +1,8 @@
 document.getElementById("spinner").style.display = 'none'
+const detailsContainer = document.getElementById("details-container")
+detailsContainer.style.display = 'none';
 const allplayers = async () => {
+    document.getElementById("details-container").style.display = 'none'
     document.getElementById("spinner").style.display = 'block'
     const searchValue = document.getElementById("search-value");
     const searchText = searchValue.value
@@ -23,23 +26,27 @@ const allplayers = async () => {
 }
 const displayPlayer = (players) => {
     for(const player of players){
-        const parent = document.getElementById("player-container")
-        const div = document.createElement("div");
-        div.innerHTML = `
-        <div class="card border text-center mt-5 delete-card">
-        <div class="pro-pic">
-            <img width="200px" src="${player.strThumb}" alt="" class="img-fluid rounded-3">
-        </div>
-            <h1>Name: ${player.strPlayer}</h1>
-            <h5>Country: ${player.strNationality}</h5>
-            <p></p>
-            <div class="allbutton">
-                <button onclick="getDetails('${player.idPlayer}')" class="btn btn-success"><a class="text-white text-decoration-none" href="#details-container">Details</a></button>
-                <button class="btn btn-danger" id="click" >Delete</button>
+        // if(player.strThumb == null){
+        //     document.getElementById("player-img").innerHTML = "";
+        //     document.getElementById("player-img").innerHTML = `<h3 class="text-muted py-5">No Picture</h3>`
+        // }
+            const parent = document.getElementById("player-container")
+            const div = document.createElement("div");
+            div.innerHTML = `
+            <div class="card w-50 text-white bg-dark rounded-3 py-3 m-auto text-center mt-5 delete-card">
+            <div class="pro-pic" id="player-img">
+                <img width="200px" src="${player.strThumb}" alt="" class="img-fluid rounded-3">
             </div>
-        </div>
-        `
-        parent.appendChild(div);
+                <h1>Name: ${player.strPlayer}</h1>
+                <h5>Country: ${player.strNationality}</h5>
+                <p></p>
+                <div class="allbutton">
+                <a class="text-white text-decoration-none" href="#details-container"><button onclick="getDetails('${player.idPlayer}')" class="btn btn-success">Details</button></a>
+                    <button class="btn btn-danger" id="click" >Delete</button>
+                </div>
+            </div>
+            `
+            parent.appendChild(div);
         
         
     }
@@ -51,7 +58,6 @@ const displayPlayer = (players) => {
             }
         }) 
 }
-
 const getDetails = async (info) => {
     document.getElementById("spinner").style.display = 'block'
     const url = `https://www.thesportsdb.com/api/v1/json/2/lookupplayer.php?id=${info}`
@@ -63,13 +69,19 @@ const getDetails = async (info) => {
 const searchPlayer = (details) => {
     console.log(details)
     document.getElementById("details-container").innerHTML = `
-    <h2 class="text-center mt-2 text-success" id="details-title">Plyer Details</h2>
+    <h2 class="text-center mt-2 text-success" id="details-title">Player Details</h2>
+    <span id="close" style="cursor: pointer;" class="position-absolute end-0 top-0 p-3 cursor-pointer">&#10060;</span>
     <div class="d-flex flex-column justify-content-center align-items-center">
         <img width="500px" src="${details.strThumb}" alt="" class="img-fluid rounded-3">
         <h1>Name: ${details.strPlayer}</h1>
         <p>Date of Birth: ${details.dateBorn}</p>
         <p>Country: ${details.strNationality}</p>
+        <p>Gender: ${details.strGender}</p>
     </div>
     `
+    document.getElementById("close").addEventListener("click",() => {
+        document.getElementById("details-container").style.display = 'none';
+    })
+    document.getElementById("details-container").style.display = 'block';
 }
 
